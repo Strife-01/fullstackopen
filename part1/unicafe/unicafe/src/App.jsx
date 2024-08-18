00858fd8c7148dd1bd376d11df28daf58dd1bb74
Andrei Ursachi <strife@Andreis-Laptop.local>
2024-08-18 21:05:30 +0200

    completed the unicafe part 1

---
+++ part1/unicafe/unicafe/src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import Button from './button.jsx'
+
+function App() {
+  const [good, setGood] = useState(() => {
+    const goodCount = localStorage.getItem('good'); 
+    if (goodCount === undefined) {
+      localStorage.setItem('good', 0);
+    } else {
+      return goodCount;
+    }
+    return 0;
+  });
+  const [neutral, setNeutral] = useState(() => {
+    const neutralCount = localStorage.getItem('neutral'); 
+    if (neutralCount === undefined) {
+      localStorage.setItem('neutral', 0);
+    } else {
+      return neutralCount;
+    }
+    return 0;
+  });
+  const [bad, setBad] = useState(() => {
+    const badCount = localStorage.getItem('bad'); 
+    if (badCount === undefined) {
+      localStorage.setItem('bad', 0);
+    } else {
+      return badCount;
+    }
+    return 0;
+  });
+  
+  const handleGood = () => {
+    setGood((goodCount) => {
+      ++goodCount;
+      localStorage.setItem("good", goodCount);
+      return goodCount;
+    });
+  }
+  const handleNeutral = () => {
+    setNeutral((neutralCount) => {
+      ++neutralCount;
+      localStorage.setItem("neutral", neutralCount);
+      return neutralCount;
+    });
+  }
+  const handleBad = () => {
+    setBad((badCount) => {
+      ++badCount;
+      localStorage.setItem("bad", badCount);
+      return badCount;
+    });
+  }
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <br/>
+      <Button buttonText={'good'} handleButton={handleGood}/>
+      <Button buttonText={'neutral'} handleButton={handleNeutral}/>
+      <Button buttonText={'bad'} handleButton={handleBad}/>
+      <br/>
+      <h1>statistics</h1>
+      <br/>
+      <p>good {good || 0}</p>
+      <p>neutral {neutral || 0}</p>
+      <p>bad {bad || 0}</p>
+    </>
+  )
+}
+
+export default App;
---
+++ part1/unicafe/unicafe/src/button.jsx
@@ -0,0 +1,5 @@
+const Button = ({buttonText, handleButton}) => {
+  return <button onClick={handleButton}>{buttonText}</button>
+}
+
+export default Button;
