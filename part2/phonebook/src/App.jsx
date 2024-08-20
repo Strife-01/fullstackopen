f8cf07a6784fae3820430e6ca5b85677d5d387bb
Andrei Ursachi <strife@Andreis-Laptop.local>
2024-08-20 22:04:25 +0200

    completed the phonebook exercise 3 from part 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,33 +2,47 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '040-1234567'}
   ]) 
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    
     const name = e.target.name.value;
+    const phone = e.target.phone.value;
+    
     let isInList = false;
     
     // Validate the input
+    if (name === '') {
+      window.alert(`Name cannot be empty!`);
+    }
+
     persons.forEach(person => {
       if (person.name === name) {
         window.alert(`${name} is already in the list!`);
         isInList = true;
       }
     });
-
-    if (isInList === false) {
-      setPersons(persons.concat({name: name}));
+      
+    if (isInList === false && name != '') {
+      setPersons(persons.concat({name: name, phone: phone}));
     }
 
     setNewName('');
+    setNewPhone('');
     e.target.name.value = '';
+    e.target.phone.value = '';
   }
 
   const handleChangeInputName = (e) => {
     setNewName(() => e.target.value);
+  }
+  
+  const handleChangeInputPhone = (e) => {
+    setNewPhone(() => e.target.value);
   }
 
   return (
@@ -39,15 +53,18 @@ const App = () => {
           name: <input onChange={handleChangeInputName} name="name" />
         </div>
         <div>
+          phone: <input onChange={handleChangeInputPhone} name="phone"/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => <div>{person.name}</div>)}
+        {persons.map((person) => <div>{person.name} {person.phone}</div>)}
       </div>
 
-      <div>debug: {newName}</div>
+      <div>debug: {newName} {newPhone}</div>
     </div>
   )
 }
