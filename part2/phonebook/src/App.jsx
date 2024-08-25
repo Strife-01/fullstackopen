18fe1ecfb76ad74e7669b8475eea09f394820252
Andrei Ursachi <strife@Andreis-Laptop.local>
2024-08-25 12:17:18 +0200

    completed the phonebook exercise 4 from part 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-1234567'}
-  ]) 
+  const [persons, setPersons] = useState(() => [
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [personsToShow, setPersonsToShow] = useState([]);
+  
   const [newName, setNewName] = useState('');
-  const [newPhone, setNewPhone] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [showAll, setShowAll] = useState(true);
+  let personNextId = useRef(5);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     
     const name = e.target.name.value;
-    const phone = e.target.phone.value;
+    const number = e.target.number.value;
     
     let isInList = false;
     
@@ -28,32 +35,58 @@ const App = () => {
     });
       
     if (isInList === false && name != '') {
-      setPersons(persons.concat({name: name, phone: phone}));
+      setPersons(persons.concat({name: name, number: number, id: personNextId.current}));
+      personNextId.current++;
     }
 
     setNewName('');
-    setNewPhone('');
+    setNewNumber('');
     e.target.name.value = '';
-    e.target.phone.value = '';
+    e.target.number.value = '';
+  }
+
+  const handleSearch = (e) => {
+    const s = e.target.value.toLowerCase();
+    setShowAll(false);
+    if (s === '') {
+      setShowAll(true);
+      setPersonsToShow([]);
+    } else {
+      const tempPersons = [];
+      persons.forEach(person => {
+        if (person.name.toLowerCase().includes(s)) {
+          tempPersons.push(person);
+        }
+      });
+      setPersonsToShow(tempPersons);
+      setShowAll(false);
+    }
   }
 
   const handleChangeInputName = (e) => {
     setNewName(() => e.target.value);
   }
   
-  const handleChangeInputPhone = (e) => {
-    setNewPhone(() => e.target.value);
+  const handleChangeInputNumber = (e) => {
+    setNewNumber(() => e.target.value);
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input onChange={handleSearch} name="search"/>
+        </div>
+      </form>
+
+      <h2>Add new number</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input onChange={handleChangeInputName} name="name" />
         </div>
         <div>
-          phone: <input onChange={handleChangeInputPhone} name="phone"/>
+          number: <input onChange={handleChangeInputNumber} name="number"/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -61,10 +94,16 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => <div>{person.name} {person.phone}</div>)}
+        {
+          showAll ? (
+            persons.map((person) => <div key={person.id}>{person.name} {person.number}</div>)
+          ) : (
+            personsToShow.map((person) => <div key={person.id}>{person.name} {person.number}</div>)
+          )
+        }
       </div>
 
-      <div>debug: {newName} {newPhone}</div>
+      <div>debug: {newName} {newNumber}</div>
     </div>
   )
 }
