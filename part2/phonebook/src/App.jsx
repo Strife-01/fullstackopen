52d4fd6259cf2257fef5999823ed4b7406777966
Andrei Ursachi <strife@Andreis-Laptop.local>
2024-09-01 13:18:03 +0200

    completed the phonebook exercise 6 from part 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,52 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
+import axios from 'axios'
 import PersonForm from './components/personform.jsx'
 import Filter from './components/filter.jsx'
 import Persons from './components/persons.jsx'
 
+const URL = 'http://localhost:3001/persons';
+
 const App = () => {
+  /*
   const [persons, setPersons] = useState(() => [
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+  */
+  const [persons, setPersons] = useState(() => []);
   const [personsToShow, setPersonsToShow] = useState([]);
   const [showAll, setShowAll] = useState(true);
   
   let personNextId = useRef(5);
 
+  useEffect(() => {
+    async function fetchData() {
+    /*
+      try {
+        const data = await fetch(URL);
+        const json_data = await data.json();
+        setPersons(json_data);
+      } catch (error) {
+        console.log(error)
+      }
+
+    */
+    
+      try {
+        const data = await axios.get(URL);
+        setPersons(data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    
+    }
+    
+    fetchData();
+  
+  }, [])
+  
   
   return (
     <div>
